f781fa048d8afacbe39f0ee76163b0c33a1cc745
daheeahn <38369729+daheeahn@users.noreply.github.com>
2020-05-09 15:55:28 +0900

    refact 함수화 (#2)
    Co-authored-by: daheeahn <deg9810@gmailcom>

--- calculator.py
+++ calculator.py
@@ -1,11 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    return a - b
+
+
+def print_result(result):
+    print(f'결과: {result}')
+
+
 if __name__ == "__main__":
-    print('원하는 연산을 입력하세요. +, -, *, %')
+    print('원하는 연산을 입력하세요. +, -')
     op = input()
+    print('숫자 2개를 입력해주세요')
+    a, b = map(int, input().split())
+
     if op == '+':
-        print('숫자 2개를 입력해주세요')
-        a, b = input().split()
-        print(f'결과: {int(a) + int(b)}')
+        print_result(add(a, b))
     elif op == '-':
-        print('숫자 2개를 입력해주세요')
-        a, b = input().split()
-        print(f'결과: {int(a) - int(b)}')
+        print_result(substract(a, b))
