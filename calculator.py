9a15cf93cb859490032a8915a59d4dca70c08725
daheeahn <deg9810@gmailcom>
2020-05-09 15:42:17 +0900

    + 연산 기능 추가

--- calculator.py
+++ calculator.py
@@ -1,2 +1,7 @@
 if __name__ == "__main__":
     print('원하는 연산을 입력하세요. +, -, *, %')
+    op = input()
+    if op == '+':
+        print('숫자 2개를 입력해주세요')
+        a, b = input().split()
+        print(f'결과: {int(a)+ int(b)}')
